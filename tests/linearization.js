fe2f10cea969dd2486f74cf202f87023de68ce2b
sdeibel1 <deibel.sebastian@gmail.com>
2019-07-02 11:13:37 -0500

    Fixed procedure argument bug

--- tests/linearization.js
+++ tests/linearization.js
@@ -332,7 +332,7 @@ Blockly.Linearization.prototype.makePartialStackItem_ = function(stack) {
 
 /**
  * Generates the html li that contains listings for all items in the stack
- * @param {!Blockly.ASTNode} stack the stack to represent
+ * @param {!Blockly.ASTNode} stackNode the stack to represent
  * @return {HTMLElement} a list element describing the complete stack as
  * a color-coded, linked sublist
  */
@@ -401,6 +401,63 @@ Blockly.Linearization.prototype.drawListForBlock = function(blockNode,
       .map(node => this.drawListForBlock(node, rootBlock, nestedItemList));
     stackItemList.append(nestedItemList);
   }
+  if (this.getNestingBlockName(block)) {
+    var endElem = Blockly.Linearization.makeListTextItem_(
+        'end ' + this.getNestingBlockName(block));
+    stackItemList.append(endElem);
+  }
+}
+
+/*
+* THIS IS BROKEN
+*/
+Blockly.Linearization.prototype.drawListForBlock2 = function(blockNode,
+    rootBlock, stackItemList) {
+  var block = blockNode.getLocation();
+  if (blockNode.getType() === Blockly.ASTNode.types.BLOCK
+    && !(block.outputConnection && block.getParent())
+    && block.getRootBlock() === rootBlock) {
+
+    if (block.type === 'controls_if') {
+      var listElems = this.makeIfListItems_(blockNode);
+      var childNodes = this.getIfChildrenNodes_(blockNode);   
+      for (var i=0; i < listElems.length; i++) {
+        if (block.getSurroundParent()) {
+          listElems[i].setAttribute('aria-label', listElems[i].innerHTML
+          + ', inside ' + this.getNestingBlockName(block.getSurroundParent()));
+        }
+        listElems[i].removeChild(listElems[i].childNodes[1]);
+        var nestedItemList = document.createElement('ul');
+        if (childNodes[i]) {
+          childNodes[i].sequence(n => n.getFirstSiblingBlock())
+            .map(node => this.drawListForBlock(node, rootBlock, nestedItemList));
+        }
+        listElems[i].append(nestedItemList);
+        stackItemList.append(listElems[i]);
+
+      }    
+      // for (var child of childNodes) {
+      //   var nestedItemList = document.createElement('ul');
+      //   blockNode.getFirstNestedBlock().sequence(n => n.getFirstSiblingBlock())
+      //     .map(node => this.drawListForBlock(node, rootBlock, nestedItemList));
+      //   stackItemList.append(nestedItemList);
+      // }
+    } else {
+      var listElem = this.makeBasicListItem_(blockNode);
+      if (block.getSurroundParent()) { 
+        listElem.setAttribute('aria-label', listElem.innerHTML
+          + ', inside ' + this.getNestingBlockName(block.getSurroundParent()));
+      }
+      stackItemList.append(listElem);   
+      if (blockNode.getFirstNestedBlock()) {
+        var nestedItemList = document.createElement('ul');
+        blockNode.getFirstNestedBlock().sequence(n => n.getFirstSiblingBlock())
+          .map(node => this.drawListForBlock(node, rootBlock, nestedItemList));
+        stackItemList.append(nestedItemList);
+      }
+    }
+  }
+
   if (this.getNestingBlockName(block)) {
     var endElem = Blockly.Linearization.makeListTextItem_(
         'end ' + this.getNestingBlockName(block));
@@ -590,8 +647,9 @@ Blockly.Linearization.prototype.makeAllMutatorItems_ = function(rootNode) {
       while (block.arguments_.includes(argname)) {
         argname += 'I';
       }
-
+      var newVar = workspace.createVariable(argname);
       block.arguments_.push(argname);
+      block.argumentVarModels_.push(newVar);
       block.updateParams_();
       this.listItemOnclick(rootNode);
     }));
@@ -607,8 +665,8 @@ Blockly.Linearization.prototype.makeAllMutatorItems_ = function(rootNode) {
           block.updateParams_();
           listItemOnclick(rootNode);
         } else {
-          block.arguments_.splice(
-            block.arguments_.indexOf(arg), 1, elem.innerText);
+          var argModel = block.getVarModels()[block.arguments_.indexOf(arg)];
+          workspace.renameVariableById(argModel.getId(), elem.innerText);
           block.updateParams_();
         }
       });
@@ -804,6 +862,26 @@ Blockly.Linearization.prototype.makeNodeListItems_ = function(node) {
   return list;
 }
 
+Blockly.Linearization.prototype.getIfChildrenNodes_ = function(ifNode) {
+  const children = ifNode.in().sequence(n => n.next());
+  const inputs = ifNode.getLocation().inputList;
+  var childrenNodes = [];
+  for (var i = 0; i < inputs.length; i += 2) {
+    if (i == inputs.length - 1) {
+      var index = i;
+    } else {
+      var index = i+1;
+    }
+
+    if (children[index].in()) {
+      childrenNodes.push(children[index].in().next());
+    } else {
+      childrenNodes.push(null);
+    }
+  }
+  return childrenNodes;
+}
+
 /**
  * Returns an ordered Array of linked html list items that represent the
  * list of branches on the if block the node contain
@@ -849,7 +927,7 @@ Blockly.Linearization.prototype.makeIfListItems_ = function(node) {
           bracketItem = this.makeBasicConnListItem_(condConnNode);
           bracketItem.innerHTML = text + ' (click to fill blank)';
         } catch(e) {
-          console.log(e);
+          console.log('text node');
           bracketItem = Blockly.Linearization.makeListTextItem_(text);
         }
     }
@@ -1151,7 +1229,7 @@ Blockly.Linearization.nextInlineInput = function(node) {
 //   }
 //   return connectionList;
 // }
-//
+
 // function getAllValidNodesForBlock(block) {
 //  var ws = Blockly.getMainWorkspace();
 //  var defaultCoord = new goog.math.Coordinate(100,100);
@@ -1163,13 +1241,13 @@ Blockly.Linearization.nextInlineInput = function(node) {
 //  } while (curNode);
 //  return nodes;
 // }
-//
+
 // Unused code
-//
-// /**
-//  * Returns all blocks in the main workspace encapsulated in nodes.
-//  * @return {Array<Blockly.ASTNode>} all possible nodes from the main workspace
-//  */
+
+/**
+ * Returns all blocks in the main workspace encapsulated in nodes.
+ * @return {Array<Blockly.ASTNode>} all possible nodes from the main workspace
+ */
 // function getAllNodes() {
 //     var ws = Blockly.getMainWorkspace();
 //     var curNode = Blockly.ASTNode.createWorkspaceNode(ws, new goog.math.Coordinate(100,100));
@@ -1180,7 +1258,7 @@ Blockly.Linearization.nextInlineInput = function(node) {
 //     } while (curNode);
 //     return nodes;
 // }
-//
+
 // /**
 //  * Decides what nodes to traverse and which ones to skip. Currently, it
 //  * skips output, stack and workspace nodes.
@@ -1199,7 +1277,7 @@ Blockly.Linearization.nextInlineInput = function(node) {
 //   }
 //   return isValid;
 // }
-//
+
 // /**
 //  * From the given node find either the next valid sibling or parent.
 //  * @param {Blockly.ASTNode} node The current position in the ast.
@@ -1217,7 +1295,7 @@ Blockly.Linearization.nextInlineInput = function(node) {
 //   }
 //   return findSiblingOrParent(node.out());
 // }
-//
+
 // /**
 //  * Uses pre order traversal to go navigate the blockly ast. This will allow
 //  * a user to easily navigate the entire blockly AST without having to go in
